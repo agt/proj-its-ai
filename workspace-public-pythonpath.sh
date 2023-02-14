f7a06ff5f1795b81876530650e0426bc5105e63b
Adam Tilghman <agt@ucsd.edu>
2023-02-13 17:52:54 -0800

    shift pythonpath setting to umbrella

--- workspace-public-pythonpath.sh
+++ workspace-public-pythonpath.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 PUBLIC=$HOME/public
+DPD=$PUBLIC/etc/datahub-profile.d
 
-if [ -d $PUBLIC ]; then
-    pydir=$(ls -1d $PUBLIC/lib/python* | head -1)
-    if [ -d "$pydir" ]; then 
-        export PYTHONPATH="${pydir}:${PYTHONPATH}"
-    fi
+if [ -d $DPD ]; then
+    for i in $DPD/*.sh ; do
+        if [ -r "$i" ]; then
+            # check bash shell options ("$-") for a lower case "i" = interactive
+            if [ "${-#*i}" != "$-" ]; then
+                . "$i"
+            else
+                . "$i" >/dev/null
+            fi
+        fi
+    done
 fi
